d8599da7185f104548438fca86edeab48c8451b3
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-04-27 14:09:12 -0700

    db.js file started

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -4,7 +4,7 @@ const request = indexedDB.open('workoutTracker', 1)
 
 ;(request.onupgradeneeded = (event) => {
     const db = event.target.result
-    db.createObjectWorkout('workout', { autoIncrement: true })
+    db.createObjectStore('workout', { autoIncrement: true })
 }),
     (request.onsuccess = function (event) {
         db = event.target.result
@@ -19,12 +19,48 @@ request.onerror = function (event) {
 }
 
 function saveRecord(record) {
-    // create a transaction on the pending db with readwrite access
-    const transaction = db.transaction(['pending'], 'readwrite')
+    // create an entry on the pending db with readwrite access
+    const entry = db.entry(['workout'], 'readwrite')
 
-    // access your pending object store
-    const store = transaction.objectStore('pending')
+    // access your pending object store of entry
+    const store = entry.objectStore('workout')
 
     // add record to your store with add method.
     store.add(record)
 }
+
+function checkDatabase() {
+    // open an entry on your pending db
+    const entry = db.transaction(['workout'], 'readwrite')
+    // access your pending object store
+    const store = entry.objectStore('workout')
+    // get all records from store and set to a variable
+    const getAll = store.getAll()
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/workouts', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then(() => {
+                    // if successful, open a transaction on your pending db
+                    const entry = db.entry(['workout'], 'readwrite')
+
+                    // access your pending object store
+                    const store = entry.objectStore('workout')
+
+                    // clear all items in your store
+                    store.clear()
+                })
+        }
+    }
+}
+
+// listen for app coming back online
+window.addEventListener('online', checkDatabase)
