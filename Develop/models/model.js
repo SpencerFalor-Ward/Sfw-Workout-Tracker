c155d9f2e66d59073f5ca753f28d49a7d2b68555
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-04-27 13:59:28 -0700

    added npm packages and model file

---
+++ Develop/models/model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const modelSchema = new Schema({
+    type: {
+        type: String,
+        // tirm: true,
+    },
+    name: {
+        type: String,
+        tirm: true,
+        required: 'Enter name of exercise',
+    },
+    duration: {
+        type: Number,
+        required: 'Enter an amount of time for exercise',
+    },
+    weight: {
+        type: Number,
+        required: 'Enter an amount for weight',
+    },
+    reps: {
+        type: Number,
+        required: 'Enter the number of reps',
+    },
+    sets: {
+        type: Number,
+        required: 'Enter the number of sets',
+    },
+    distance: {
+        type: Number,
+        required: 'Enter the distance desired',
+    },
+
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+const Model = mongoose.model('Model', modelSchema)
+
+module.exports = Model
