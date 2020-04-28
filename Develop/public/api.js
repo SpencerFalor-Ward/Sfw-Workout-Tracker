07d41b5dec9fcaddf451da392dca5853188f9b2f
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-04-27 18:39:24 -0700

    updating api and apiRoute files and seed file fix

--- Develop/public/api.js
+++ Develop/public/api.js
@@ -42,36 +42,3 @@ const API = {
         return json
     },
 }
-
-router.post('/api/transaction', ({ body }, res) => {
-    Model.create(body)
-        .then((dbTransaction) => {
-            res.json(dbTransaction)
-        })
-        .catch((err) => {
-            res.status(400).json(err)
-        })
-})
-
-router.post('/api/transaction/bulk', ({ body }, res) => {
-    Model.insertMany(body)
-        .then((dbTransaction) => {
-            res.json(dbTransaction)
-        })
-        .catch((err) => {
-            res.status(400).json(err)
-        })
-})
-
-router.get('/api/transaction', (req, res) => {
-    Model.find({})
-        .sort({ date: -1 })
-        .then((dbTransaction) => {
-            res.json(dbTransaction)
-        })
-        .catch((err) => {
-            res.status(400).json(err)
-        })
-})
-
-module.exports = router
--- Develop/routes/apiRoute.js
+++ Develop/routes/apiRoute.js
@@ -1,76 +1,31 @@
 const router = require('express').Router()
 const Model = require('../models/model.js')
 
-const API = {
-    async getLastWorkout() {
-        let res
-        try {
-            res = await fetch('/api/workouts')
-        } catch (err) {
-            console.log(err)
-        }
-        const json = await res.json()
-
-        return json[json.length - 1]
-    },
-    async addExercise(data) {
-        const id = location.search.split('=')[1]
-
-        const res = await fetch('/api/workouts/' + id, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data),
-        })
-
-        const json = await res.json()
-
-        return json
-    },
-    async createWorkout(data = {}) {
-        const res = await fetch('/api/workouts', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: { 'Content-Type': 'application/json' },
-        })
-
-        const json = await res.json()
-
-        return json
-    },
-
-    async getWorkoutsInRange() {
-        const res = await fetch(`/api/workouts/range`)
-        const json = await res.json()
-
-        return json
-    },
-}
-
-router.post('/api/transaction', ({ body }, res) => {
+router.post('/api/workouts', ({ body }, res) => {
     Model.create(body)
-        .then((dbTransaction) => {
-            res.json(dbTransaction)
+        .then((dbWorkout) => {
+            res.json(dbWorkout)
         })
         .catch((err) => {
             res.status(400).json(err)
         })
 })
 
-router.post('/api/transaction/bulk', ({ body }, res) => {
+router.post('/api/workouts/bulk', ({ body }, res) => {
     Model.insertMany(body)
-        .then((dbTransaction) => {
-            res.json(dbTransaction)
+        .then((dbWorkout) => {
+            res.json(dbWorkout)
         })
         .catch((err) => {
             res.status(400).json(err)
         })
 })
 
-router.get('/api/transaction', (req, res) => {
+router.get('/api/workouts', (req, res) => {
     Model.find({})
         .sort({ date: -1 })
-        .then((dbTransaction) => {
-            res.json(dbTransaction)
+        .then((dbWorkout) => {
+            res.json(dbWorkout)
         })
         .catch((err) => {
             res.status(400).json(err)
