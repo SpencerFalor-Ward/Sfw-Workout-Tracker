192b6e0b477e13c2943d3979142d2f742062c130
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-04-28 11:57:44 -0700

    fixed model file and aiRoutes

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -1,66 +1,66 @@
-let db
+// let db
 
-const request = indexedDB.open('workoutTracker', 1)
+// const request = indexedDB.open('workoutTracker', 1)
 
-;(request.onupgradeneeded = (event) => {
-    const db = event.target.result
-    db.createObjectStore('workout', { autoIncrement: true })
-}),
-    (request.onsuccess = function (event) {
-        db = event.target.result
+// ;(request.onupgradeneeded = (event) => {
+//     const db = event.target.result
+//     db.createObjectStore('workout', { autoIncrement: true })
+// }),
+//     (request.onsuccess = function (event) {
+//         db = event.target.result
 
-        // check if app is online before reading from db
-        if (navigator.onLine) {
-            checkDatabase() //needs new name and alter the function
-        }
-    })
-request.onerror = function (event) {
-    console.log('Woops! ' + event.target.errorCode)
-}
+//         // check if app is online before reading from db
+//         if (navigator.onLine) {
+//             checkDatabase() //needs new name and alter the function
+//         }
+//     })
+// request.onerror = function (event) {
+//     console.log('Woops! ' + event.target.errorCode)
+// }
 
-function saveRecord(record) {
-    // create an entry on the pending db with readwrite access
-    const entry = db.entry(['workout'], 'readwrite')
+// function saveRecord(record) {
+//     // create an entry on the pending db with readwrite access
+//     const entry = db.entry(['workout'], 'readwrite')
 
-    // access your pending object store of entry
-    const store = entry.objectStore('workout')
+//     // access your pending object store of entry
+//     const store = entry.objectStore('workout')
 
-    // add record to your store with add method.
-    store.add(record)
-}
+//     // add record to your store with add method.
+//     store.add(record)
+// }
 
-function checkDatabase() {
-    // open an entry on your pending db
-    const entry = db.transaction(['workout'], 'readwrite')
-    // access your pending object store
-    const store = entry.objectStore('workout')
-    // get all records from store and set to a variable
-    const getAll = store.getAll()
+// function checkDatabase() {
+//     // open an entry on your pending db
+//     const entry = db.transaction(['workout'], 'readwrite')
+//     // access your pending object store
+//     const store = entry.objectStore('workout')
+//     // get all records from store and set to a variable
+//     const getAll = store.getAll()
 
-    getAll.onsuccess = function () {
-        if (getAll.result.length > 0) {
-            fetch('/api/workouts', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json',
-                },
-            })
-                .then((response) => response.json())
-                .then(() => {
-                    // if successful, open a transaction on your pending db
-                    const entry = db.entry(['workout'], 'readwrite')
+//     getAll.onsuccess = function () {
+//         if (getAll.result.length > 0) {
+//             fetch('/api/workouts', {
+//                 method: 'POST',
+//                 body: JSON.stringify(getAll.result),
+//                 headers: {
+//                     Accept: 'application/json, text/plain, */*',
+//                     'Content-Type': 'application/json',
+//                 },
+//             })
+//                 .then((response) => response.json())
+//                 .then(() => {
+//                     // if successful, open a transaction on your pending db
+//                     const entry = db.entry(['workout'], 'readwrite')
 
-                    // access your pending object store
-                    const store = entry.objectStore('workout')
+//                     // access your pending object store
+//                     const store = entry.objectStore('workout')
 
-                    // clear all items in your store
-                    store.clear()
-                })
-        }
-    }
-}
+//                     // clear all items in your store
+//                     store.clear()
+//                 })
+//         }
+//     }
+// }
 
-// listen for app coming back online
-window.addEventListener('online', checkDatabase)
+// // listen for app coming back online
+// window.addEventListener('online', checkDatabase)
--- Develop/routes/apiRoute.js
+++ Develop/routes/apiRoute.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const Workout = require('../models/model.js')
 
 router.post('/api/workouts', ({ body }, res) => {
+    console.log(body)
     Workout.create(body)
         .then((dbWorkout) => {
             res.json(dbWorkout)
@@ -9,17 +10,9 @@ router.post('/api/workouts', ({ body }, res) => {
         .catch((err) => {
             res.status(400).json(err)
         })
-    // Cardio.create(body)
-    //     .then((dbWorkout) => {
-    //         res.json(dbWorkout)
-    //     })
-    //     .catch((err) => {
-    //         res.status(400).json(err)
-    //     })
 })
-
-router.post('/api/workouts/bulk', ({ body }, res) => {
-    Workout.insertMany(body)
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+    Workout.updateOne({ _id: params.id }, { $push: { exercises: body } })
         .then((dbWorkout) => {
             res.json(dbWorkout)
         })
@@ -28,10 +21,42 @@ router.post('/api/workouts/bulk', ({ body }, res) => {
         })
 })
 
-router.get('/api/workouts', (req, res) => {
+router.post('/api/workouts/bulk', ({ body }, res) => {
+    Workout.insertMany(body)
+        .then((dbWorkout) => {
+            console.log('line 17 called')
+            console.log(dbWorkout)
+            res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.status(400).json(err)
+        })
+})
+
+// router.get('/api/workouts', (req, res) => {
+//     res.json([])
+// })
+
+router.get('/api/workouts/', (req, res) => {
+    // res.json([])
     Workout.find({})
         .sort({ date: -1 })
         .then((dbWorkout) => {
+            console.log('line 29 called')
+            console.log(dbWorkout)
+            res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.status(400).json(err)
+        })
+})
+router.get('/api/workouts/range', (req, res) => {
+    // res.json([])
+    Workout.find({})
+        .limit(7)
+        .then((dbWorkout) => {
+            console.log('line 29 called')
+            console.log(dbWorkout)
             res.json(dbWorkout)
         })
         .catch((err) => {
